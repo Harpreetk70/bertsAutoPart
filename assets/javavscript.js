af3f7eac85770acafaddf4e7fb94d6838f89ec88
Sandip Singh <sandip.singh@ucodesoft.com>
2020-12-22 19:51:16 +0530

    Harpreet First Commit

---
+++ assets/javavscript.js
@@ -0,0 +1,7 @@
+// $('.countr').on('click', '.add, .subtract', function(e) {
+//   var clicked = this;
+//   $(this).siblings('input').val(function(i, v) {
+//     return parseInt(v, 10) + (clicked.classList.contains('add') ? 1 : -1);
+//   });
+// });
+
